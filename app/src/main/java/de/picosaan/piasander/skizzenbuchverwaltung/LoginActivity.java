5350ce6e966681875c85997c5e63cc46ff1f88aa
Pia Sander <specialnightstar91@googlemail.com>
2018-05-12 18:29:41 +0200

    Login fertig

--- app/src/main/java/de/picosaan/piasander/skizzenbuchverwaltung/LoginActivity.java
+++ app/src/main/java/de/picosaan/piasander/skizzenbuchverwaltung/LoginActivity.java
@@ -1,13 +1,284 @@
 package de.picosaan.piasander.skizzenbuchverwaltung;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mOptionalStatus;
+    private ProgressBar mProgressBar;
+
+    private boolean signingIn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(savedInstanceState != null){
+            signingIn = savedInstanceState.getBoolean("signingIn");
+        }
+
+
+        // Views
+        mOptionalStatus = (TextView) findViewById(R.id.optional_status);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //https://console.developers.google.com/apis/credentials?project=skizzenbuecher&authuser=0 Webanwendung, nicht Android!
+                .requestIdToken("256383817889-n3bmsn626qoh3mclf3gqcq4d00gikr6s.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
+        //If signing in was cancelled, it gets restarted
+        if(signingIn){
+            signIn();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        if (currentUser != null) {
+            if (message != null && message.equals("signout")) {
+
+                final Thread thread = new Thread() {
+
+                    public void run() {
+
+                        if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                            mGoogleApiClient.connect();
+                        }
+
+                        while (!mGoogleApiClient.isConnected()) {
+                            try {
+                                Thread.currentThread().sleep(50);
+                            } catch (InterruptedException e) {
+
+                            }
+                        }
+                        if (mGoogleApiClient.isConnected()) {
+                            signOut();
+                        }
+                    }
+
+
+                };
+                thread.start();
+
+            } else
+
+                startSketchbookActvity();
+
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+
+            showSignInMessage();
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(LoginActivity.this, getResources().getString(R.string.auth_failed),
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
+    }
+
+    // START auth_with_google
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        showSignInMessage();
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            startSketchbookActvity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, getResources().getString(R.string.auth_failed),
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+
+                        hideSignInMessage();
+                        signingIn = false;
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean("signingIn", signingIn);
+
+    }
+
+    //START signIn
+    private void signIn() {
+        signingIn = true;
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    //START signOut
+    private void signOut() {
+
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+
+    private void startSketchbookActvity() {
+
+        Toast.makeText(LoginActivity.this, getResources().getString(R.string.auth_success),
+                Toast.LENGTH_SHORT).show();
+        //startActivity(new Intent(this, DatabaseActivity.class));
+    }
+
+
+    private void showSignInMessage() {
+
+        mOptionalStatus.setText(getString(R.string.signing_in));
+        mOptionalStatus.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideSignInMessage() {
+        mProgressBar.setVisibility(View.GONE);
+        mOptionalStatus.setVisibility(View.GONE);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideSignInMessage();
+        if (user != null) {
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
     }
 }
